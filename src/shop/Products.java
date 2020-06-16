ad0a86849fe049356426c7dfd744aae0c3c96018
kolomszczyk <kolomszczyk@gmail.com>
2020-06-16 13:29:43 +0200

    ...

--- src/shop/Products.java
+++ src/shop/Products.java
@@ -1,5 +1,39 @@
 package shop;
 
 public class Products {
-
+	private String name;
+	//Picture;
+	private int price;
+	private String description;
+	
+	public Products(String name, int price, String description) {
+		this.name = name;
+		this.price = price;
+		this.description = description;
+	}
+	
+	public void setName(String newName) {
+		this.name = newName;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setPrice(int newPrice) {
+		this.price = newPrice;
+	}
+	
+	public int getPrice() {
+		return this.price;
+	}
+	
+	public void setDescription(String newDescription) {
+		this.description = newDescription;
+	}
+	
+	public String getDescriptino() {
+		return this.description;
+	}
+	
 }
--- src/shop/ProductsMenager.java
+++ src/shop/ProductsMenager.java
@@ -1,5 +1,19 @@
 package shop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductsMenager {
+	
+	private List<Products> list = new ArrayList<Products>();
+	
+	public ProductsMenager() {
+		super();
+	}
+	
+	public void addProduct(Products productToAdd) {
+		list.add(productToAdd);
+	}
+	
 
 }
